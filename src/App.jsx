9a183622126194de7561877445a225ba85d5e570
Mridul Srivastava <p3mtech@gmail.com>
2026-07-07 01:16:47 +0530

    Fixing github pages deployment
    Fixing github pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
       <div className="min-h-screen bg-gray-100">
         <AppRoutes />
       </div>
-    </BrowserRouter>
   );
 }
 
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,21 +1,23 @@
 import React, { Suspense, lazy } from 'react'
 
-import {  Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 // BrowserRouter
 const HomePage = lazy(() => import('../pages/HomePage'))
 const ResumePage = lazy(() => import('../pages/ResumePage'))
 
 const AppRoutes = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      {/* <BrowserRouter basename="/portfolio-app/"> */}
+        // <BrowserRouter basename="/portfolio-app/">
+    <HashRouter>
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes basename="/portfolio-app/">
           {/* Add more routes as needed */}
           <Route path="/" element={<HomePage />} />
           <Route path="/resume" element={<ResumePage />} />
         </Routes>
-      {/* </BrowserRouter> */}
-    </Suspense>
+      </Suspense>
+    </HashRouter>
+    //</BrowserRouter>
   )
 }
 
